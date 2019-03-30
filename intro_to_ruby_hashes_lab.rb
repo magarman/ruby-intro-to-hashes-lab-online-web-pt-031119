2a0e27e683915099203eaa565fb067e5da7ea5cd
Mari Garman <garman.mariana@gmail.com>
2019-03-30 17:44:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def new_hash
   hash = Hash.new
 end
@@ -23,11 +25,13 @@ def monopoly_with_third_tier
 	two_pieces_owned: 50, 
 	three_pieces_owned: 100, 
 	four_pieces_owned: 200}, 
+	
 	{reading_railroad: {}, 
 	pennsylvania_railroad: {}, 
 	b_and_o_railroad: {}, 
 	shortline: {} }
 	   }
+	   binding.pry
 end
 
 def monopoly_with_fourth_tier
