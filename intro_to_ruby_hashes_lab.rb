3c97820bbfdfc206c1ab4be10b8702cf19b1fda8
Mari Garman <garman.mariana@gmail.com>
2019-03-30 17:48:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,7 @@ def monopoly_with_third_tier
 	      three_pieces_owned: 100, 
 	      four_pieces_owned: 200
 	    }, 
-	    {reading_railroad: {}, 
-	pennsylvania_railroad: {}, 
-	b_and_o_railroad: {}, 
-	shortline: {} }
+	    { }
 	   }
 	   binding.pry
 end
